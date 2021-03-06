2a0c7df70c1b0c39f49ca9a30adf6e9233330e49
anandman03 <anandmansimar@gmail.com>
2021-03-06 23:07:27 +0530

    Added Selection Sort

--- js/algorithms/selectionSort.js
+++ js/algorithms/selectionSort.js
@@ -4,6 +4,16 @@ function getSelectionSortMoves(list) {
     let moves = [];
     let array = getNumericalValues(list);
 
-    
+    for(let index = 0 ; index < array.length ; ++index) {
+        let minIndex = index;
+        for(let ptr = index + 1 ; ptr < array.length ; ++ptr) {
+            if(array[ptr] < array[minIndex]) {
+                minIndex = ptr;
+            }
+            moves.push([minIndex, ptr, NO_SWAP]);
+        }
+        swap(array, minIndex, index);
+        moves.push([minIndex, index, SWAP]);
+    }
     return moves;
 };
