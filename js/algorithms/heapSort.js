535ed1c88364ce305ec66d33595abe3b47736809
anandman03 <anandmansimar@gmail.com>
2021-03-08 13:59:07 +0530

    Added Heap Sort

--- js/algorithms/heapSort.js
+++ js/algorithms/heapSort.js
@@ -4,16 +4,29 @@ function getHeapSortMoves(list) {
     let moves = [];
     let array = getNumericalValues(list);
 
-    for(let counter = 0 ; counter < (list.length - 1) ; ++counter) {
-        for(let index = 0 ; index < (list.length - counter - 1) ; ++index) {
-            if(array[index] > array[index + 1]) {
-                swap(array, index, index + 1);
-                moves.push([index, index + 1, SWAP]);
-            }
-            else {
-                moves.push([index, index + 1, NO_SWAP]);
-            }
-        }
+    for(let index = array.length - 1 ; index >= 0 ; --index) {
+        heapify(array, length, index, moves);
     }
+    for(let index = array.length - 1 ; index >= 0 ; --index) {
+        moves.push([index, 0, SWAP]);
+        swap(array, index, 0);
+        heapify(array, index, 0, moves);
+    }
+    console.log(array);
     return moves;
+};
+
+function heapify(array, length, index, moves) {
+    let largest = index, left = 2*index + 1, right = 2*index + 2;
+    if(left < length && array[left] > array[largest]) {
+        largest = left;
+    }
+    if(right < length && array[right] > array[largest]) {
+        largest = right;
+    }
+    if(largest != index) {
+        moves.push([index, largest, SWAP]);
+        swap(array, index, largest);
+        heapify(array, length, largest, moves);
+    }
 };
